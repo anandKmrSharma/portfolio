a57afdc11e144fe4440d5b86481e187a1f8e62a4
anandKmrSharma <87422058+anandKmrSharma@users.noreply.github.com>
2022-01-14 20:49:43 +0530

    Update TimeLine.js

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -27,11 +27,21 @@ const Timeline = () => {
       <br />
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-      I am Mechanical graduate Engineer with a passion of
+        {/* I am Mechanical graduate Engineer with a passion of
         developing scalable web application and working across the full stack. I
         have designed the clone of some of the websites using Node.js,
         Javascript, HTML, CSS and Mongodb. Curious to learn and apply my skills
-        on a large scale.
+        on a large scale. */}
+        When I was in college, I felt that I was more interested in doing coding and solving problems rather than
+        working in my mechanical workshop. So, I decided to learn technology. Initially, I tried to learn from
+        Youtube videos as well as from other free resources, but I felt like I needed guidance from a professional
+        if I want to pursue a career in the IT industry. This is why I decided to join Masai School to learn
+        coding.
+        I'm passionate about problem solving. I'm highly skilled in HTML, CSS, Mongodb and Express.
+
+        I would like to work in an organisation that offers opportunities where i can showcase my technical
+        skills and knowledge. I hope to work in an organisation, that allows me to learn new things on a 
+        daily basis, which would in turn help my overall growth.
       </SectionText>
 
     </Section>
