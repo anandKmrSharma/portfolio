cf1bf92e8d08124da122607f621778df2ad53cad
anandKmrSharma <87422058+anandKmrSharma@users.noreply.github.com>
2022-01-14 19:31:49 +0530

    updated

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: "Smallcase Clone",
     description:
-      "Smallcase is the website where users can buy stocks and invest.",
+      "Smallcase is the website where users can buy stocks and invest in it.",
     image: "/images/1.png",
     tags: ["HTML", "CSS", "Javascript"],
     source: "https://github.com/vishnudutta1/Mednear",
@@ -15,8 +15,8 @@ export const projects = [
       "Nykaa is an Indian e-commerce website. In this project we have cloned some important features of the website with backend integration. We used NodeJS runtime, Express framework and MongoDB as database",
     image: "/images/2.png",
     tags: ["NodeJS", "ExpressJS", "MongoDB"],
-    source: "https://github.com/vishnudutta1/unit4/tree/main/NykaaCloneWithBackend-main",
-    visit: "https://nykaa-clone-project.herokuapp.com/",
+    source: "https://github.com/gurdeep7/upgrad",
+    visit: "https://gurdeep7.github.io/upgrad/html/index.html",
     id: 1,
   },
 ];
